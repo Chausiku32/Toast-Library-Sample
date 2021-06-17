f33b05b952e1a2a50ef665a3d948e82df70c64c9
Nickson Munge <nickson.munge@tracom.co.ke>
2021-06-17 14:37:09 +0300

    Initial commit

---
+++ ToasterMessage/src/main/java/tracom/pos/toastermessage/ToastMessage.java
@@ -0,0 +1,10 @@
+package tracom.pos.toastermessage;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMessage {
+    public static void s(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/tracom/pos/toasterapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package tracom.pos.toasterapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ToastMessage.s(getApplicationContext(), "Android sample library");
+    }
+}
